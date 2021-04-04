8f41e9bbc40dfb6e843d3360bac742f4552a100b
Bowen Wu <51037053+Software-Cat@users.noreply.github.com>
2021-04-04 20:50:12 +1000

    Add package info files

---
+++ src/main/java/io/github/softwarecats/casino/event/RandomEvent.java
@@ -0,0 +1,4 @@
+package io.github.softwarecats.casino.event;
+
+public abstract class RandomEvent {
+}
---
+++ src/main/java/io/github/softwarecats/casino/player/Player.java
@@ -0,0 +1,4 @@
+package io.github.softwarecats.casino.player;
+
+public abstract class Player {
+}
